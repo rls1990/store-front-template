5a73a30fe80d1316f0a8f1db9a83aabf7514c605
rls1990 <roilanlauzardosotolongo@gmail.com>
2025-08-27 17:12:50 +0200

    NavCar count

--- src/components/layout/header/nav/NavCar.tsx
+++ src/components/layout/header/nav/NavCar.tsx
@@ -10,9 +10,11 @@ export const NavCar = () => {
     <div className="absolute right-0 top-0 flex items-center justify-center h-full mr-3">
       <Link href="/car-list" className="relative">
         <TiShoppingCart className="size-10 text-emerald-700" />
-        <span className="px-[6px] bg-emerald-700/90 absolute top-0 right-0 rounded-full shadow-md shadow-gray-700 text-gray-100 flex text-sm">
-          {count}
-        </span>
+        {count && count > 0 && (
+          <span className="px-[6px] bg-emerald-700/90 absolute top-0 right-0 rounded-full shadow-md shadow-gray-700 text-gray-100 flex text-sm">
+            {count}
+          </span>
+        )}
       </Link>
     </div>
   );
